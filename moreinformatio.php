2b10ee17619020690b4f4373006a886713980738
saipraneeth <Makhampraneeth@gmail.com>
2017-11-03 19:47:59 +0530

    Create moreinformatio.php

---
+++ moreinformatio.php
@@ -0,0 +1,25 @@
+<?php
+echo "<h1 align="center">COLLEGE MAGAZINE</h1>n";
+echo "<h2 align="center">MORE INFORAMTION</h2> n";
+echo "<p></p>n";
+echo "<div class="form"><form id="contactform">n";
+echo "<p class="contact"><label for="deparment">DEPARMENT</label></p>n";
+echo "<select>n";
+echo "<option label="Deparment">Deparment</option>n";
+echo "  <option value="CSE">CSE</option>n";
+echo "  <option value="ECE">ECE</option>n";
+echo "  <option value="MECH">MECH</option>n";
+echo "  <option value="CIVIL">CIVIL</option>n";
+echo "</select>		n";
+echo "<p class="contact"><label for="insersted area">INSERSTED AREA</label></p>n";
+echo "<select>n";
+echo "<option label="Insersted area">Insersted area</option>n";
+echo "  <option value="BIG DATA">BIG DATA</option>n";
+echo "  <option value="CLOUD">CLOUD</option>n";
+echo "  <option value="VLSI">VLSI</option>n";
+echo "  <option value="STRUCTURAL">STRUCTURAL</option>n";
+echo "</select>n";
+echo "<br><br>n";
+echo "<input id="submit" class="buttom" tabindex="5" name="submit" type="submit" value="Update" /></form></div>n";
+echo "</div>n";
+?>
